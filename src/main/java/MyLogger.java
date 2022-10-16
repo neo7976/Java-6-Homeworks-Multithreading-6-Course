02b7d6c3356a15cc608e175d5b893206f59ae07a
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-16 21:35:07 +0300

    Мой логер

--- src/main/java/MyLogger.java
+++ src/main/java/MyLogger.java
@@ -1,21 +1,44 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyLogger {
     private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-    protected int num = 1;
-    private static MyLogger instance = null;
+    protected AtomicInteger num = new AtomicInteger(0);
+    private ConcurrentHashMap<String, Integer> frag = new ConcurrentHashMap<>();
+    private static volatile MyLogger INSTANCE = null;
 
     private MyLogger() {
     }
 
-    public String log(String msg) {
-       return String.format("[Date: %s %3d] -> %s\n", dtf.format(LocalDateTime.now()), num++, msg);
+    public String log(String userName, String msg) {
+        frag.put(userName, frag.getOrDefault(userName, 0) + 1);
+        String s = String.format("[Date: %s %3d]\n -> %s%s\n", dtf.format(LocalDateTime.now()), num.incrementAndGet(), userName, msg);
+        writeLog(s);
+        return s;
     }
 
     public static MyLogger getInstance() {
-        if (instance == null)
-            instance = new MyLogger();
-        return instance;
+        if (INSTANCE == null) {
+            synchronized (MyLogger.class) {
+                if (INSTANCE == null)
+                    INSTANCE = new MyLogger();
+            }
+        }
+        return INSTANCE;
+    }
+
+    public void writeLog(String s) {
+        try {
+            BufferedWriter bf = new BufferedWriter(new FileWriter("src/main/resources/myLog.log", true));
+            bf.write(s);
+            bf.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
