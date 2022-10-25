abb732216266eeb5221c565e48a74bc1e4baa2bc
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-25 09:36:40 +0300

    Рефакторинг вывода логера для пользователей

--- src/main/java/log/MyLogger.java
+++ src/main/java/log/MyLogger.java
@@ -19,11 +19,11 @@ public class MyLogger {
 
     public String log(String userName, String msg) {
         frag.put(userName, frag.getOrDefault(userName, 0) + 1);
-        String s = String.format("[%s %3d]\n(%d)-> %s: %s\n",
+        String s = String.format("[%s %3d]\n%s (%d): %s\n",
                 dtf.format(LocalDateTime.now()),
                 num.incrementAndGet(),
-                frag.get(userName),
                 userName,
+                frag.get(userName),
                 msg);
         writeLog(s);
         return s;
